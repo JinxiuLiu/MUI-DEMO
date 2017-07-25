278d2bdebbe8366df63a3ee59a5eda9e9d90c832
Liujx <zhoumo@sayweb.top>
2017-07-25 23:52:56 +0800

    add index

---
+++ common/script/reset.js
@@ -0,0 +1,43 @@
+/**
+ * Created by Liujx on 2017-07-25 09:41:22
+ */
+;(function(designWidth, maxWidth) {
+	'use strict';
+
+    var doc = document,
+        win = window,
+        docEl = doc.documentElement,
+        oHTML = document.getElementsByTagName("html")[0],
+        tid;
+
+    function refreshRem() {
+        var width = docEl.getBoundingClientRect().width;
+        maxWidth = maxWidth || 540;
+        width>maxWidth && (width=maxWidth);
+        var rem = width * 40 / designWidth;
+        oHTML.style.fontSize = rem + 'px';
+    }
+
+    refreshRem();
+
+    win.addEventListener("resize", function() {
+        clearTimeout(tid); //防止执行两次
+        tid = setTimeout(refreshRem, 300);
+    }, false);
+
+    win.addEventListener("pageshow", function(e) {
+        if (e.persisted) { // 浏览器后退的时候重新计算
+            clearTimeout(tid);
+            tid = setTimeout(refreshRem, 300);
+        }
+    }, false);
+
+    if (doc.readyState === "complete") {
+        doc.body.style.fontSize = "16px";
+    } else {
+        doc.addEventListener("DOMContentLoaded", function(e) {
+            doc.body.style.fontSize = "16px";
+        }, false);
+    }
+
+})(750, 750);
---
+++ js/index.js
@@ -0,0 +1,48 @@
+/**
+ * Created by Liujx on 2017-07-25 16:12:42
+ */
+(function(global, $, mui, doc) {
+	'use strict';
+	
+	var indexJs = function() {
+		this.initialization();
+	}
+
+	indexJs.prototype = {
+		constructor: indexJs,
+		muiSlider: function() {
+			//获得slider插件对象
+			var gallery = mui('.mui-slider');
+			gallery.slider({
+			  interval: 5000//自动轮播周期，若为0则不自动播放，默认为0；
+			});
+		},
+		initialization: function() {	// 初始化
+			this.muiSlider();
+            this._scanEventsMap(this.eventsMap, true);
+        },
+		_scanEventsMap: function(maps, isOn) {
+            var delegateEventSplitter = /^(\S+)\s*(.*)$/;
+            var bind = isOn ? this._delegate : this._undelegate;
+            for (var keys in maps) {
+                if (maps.hasOwnProperty(keys)) {
+                    var matchs = keys.match(delegateEventSplitter);
+                    bind(matchs[1], matchs[2], maps[keys].bind(this));
+                }
+            }
+        },
+        _delegate: function(name, selector, func) {
+            doc.on(name, selector, func);
+        },
+        _undelegate: function(name, selector, func) {
+            doc.off(name, selector, func);
+        },
+	}
+
+	global.indexJs = indexJs;
+
+    $(function() {
+        new indexJs();
+    });
+
+})(this, this.jQuery, this.mui, document);
