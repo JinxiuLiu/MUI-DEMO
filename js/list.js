c19db726634092f2674313840097b0ab92c678ac
Liujx <zhoumo@sayweb.top>
2017-07-29 14:54:53 +0800

    add search

--- js/list.js
+++ js/list.js
@@ -1,3 +1,19 @@
 /**
  * Created by Liujx on 2017-07-26 21:33:10
  */
+	
+(function($) {
+	$.init();
+
+    var offCanvasInner = $('.mui-off-canvas-wrap')[0].querySelector('.mui-inner-wrap');
+
+	offCanvasInner.addEventListener('drag', function(event) {
+	    event.stopPropagation();
+	});
+	
+	$(".mui-scroll-wrapper").scroll({
+		deceleration : 0.0005
+	});
+
+})(mui)
+    
